900f0eaaa9a8734dec1a15a599fe6a581d9c4341
hudso2tj <hudsontj20@gmail.com>
2024-10-01 15:14:00 -0400

    Added some tooltip functionality.

--- FinancialApplication/Pages/SavingsCalculator.cshtml.cs
+++ FinancialApplication/Pages/SavingsCalculator.cshtml.cs
@@ -1,11 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace FinancialApplication.Pages
 {
     public class SavingsCalculatorModel : PageModel
     {
-        public void OnGet()
+
+        [BindProperty]
+        [Required]
+        [Range(0.0, double.MaxValue, ErrorMessage = "Must be greater than zero.")]
+        public double interest_rate { get; set; }
+
+        [BindProperty]
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Must be greater than zero.")]
+        public double deposit_amount { get; set; }
+
+        public void OnPost()
         {
         }
     }
