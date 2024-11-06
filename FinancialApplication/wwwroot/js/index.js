ccaf5d86f2469a943c08111bf01cc6bfb22b1ba9
hudso2tj <hudsontj20@gmail.com>
2024-11-06 18:49:37 -0500

    Progress update to the new suggestion page.

--- FinancialApplication/wwwroot/js/index.js
+++ FinancialApplication/wwwroot/js/index.js
@@ -1,4 +1,2 @@
-﻿window.onload = function () {
-    alert("Welcome to my WIP Finance Web App!\nThis site is still a work in process and tips or suggestions are deeply valued. My email is hudsontj20@gmail.com if you'd like to contact me or suggest a feature. Thanks!");
-};
+﻿
 
