5ec4314c1164933c66bfaf635680f735ac085ad8
hudso2tj <hudsontj20@gmail.com>
2024-11-25 15:08:07 -0500

    Enforced stronger data validation rules on existing forms.

--- FinancialApplication/Pages/SavingsCalculator.cshtml.cs
+++ FinancialApplication/Pages/SavingsCalculator.cshtml.cs
@@ -10,12 +10,18 @@ namespace FinancialApplication.Pages
         [BindProperty]
         [Required]
         [Range(0.0, double.MaxValue, ErrorMessage = "Must be greater than zero.")]
-        public double interest_rate { get; set; }
+        public double starting_balance { get; set; }
 
         [BindProperty]
         [Required]
         [Range(0, double.MaxValue, ErrorMessage = "Must be greater than zero.")]
-        public double deposit_amount { get; set; }
+        public double contribution_amount { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Please select a compounding frequency.")]
+        public int contribution_frequency { get; set; }
+
+
 
         public void OnPost()
         {
