0e4287c2a79e8fd2b5b63d032122a4bf8d1fc224
hudso2tj <hudsontj20@gmail.com>
2024-11-25 18:56:34 -0500

    Progress made on the Savings Calculator tab

--- FinancialApplication/Pages/SavingsCalculator.cshtml.cs
+++ FinancialApplication/Pages/SavingsCalculator.cshtml.cs
@@ -9,22 +9,40 @@ namespace FinancialApplication.Pages
 
         [BindProperty]
         [Required]
-        [Range(0.0, double.MaxValue, ErrorMessage = "Must be greater than zero.")]
+        [Range(0.0, double.MaxValue, ErrorMessage = "Please enter a number greater than 0")]
         public double starting_balance { get; set; }
 
         [BindProperty]
         [Required]
-        [Range(0, double.MaxValue, ErrorMessage = "Must be greater than zero.")]
+        [Range(0, double.MaxValue, ErrorMessage = "Please enter a number greater than 0")]
         public double contribution_amount { get; set; }
 
         [BindProperty]
-        [Required(ErrorMessage = "Please select a compounding frequency.")]
+        [Required(ErrorMessage = "Please select a compounding frequency")]
         public int contribution_frequency { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Please enter a number greater than 0")]
+        public int years {  get; set; }
+
+        [BindProperty]
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Please enter a number greater than 0")]
+        public double annual_interest { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Please select a compound frequency")]
+        public int compound_frequency { get; set; }
+
 
 
 
         public void OnPost()
         {
+            //Calculations
+
+
+
         }
     }
 }
